91795f1a483b8cf1e834dc5e54a857ad18caf69a
liaoxuewei <liaoxuewei@hotmail.com>
2025-03-08 10:48:27 +0800

    * 菜单配置类

---
+++ src/main/java/com/example/demo/EruptDemoAutoConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import com.example.demo.model.blog.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import xyz.erupt.core.annotation.EruptScan;
+import xyz.erupt.core.constant.MenuTypeEnum;
+import xyz.erupt.core.module.EruptModule;
+import xyz.erupt.core.module.EruptModuleInvoke;
+import xyz.erupt.core.module.MetaMenu;
+import xyz.erupt.core.module.ModuleInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jAmEs_
+ * date 2025/3/8
+ */
+@Configuration
+@ComponentScan
+@EntityScan
+@EruptScan
+@EnableConfigurationProperties
+public class EruptDemoAutoConfiguration implements EruptModule {
+
+    static {
+        EruptModuleInvoke.addEruptModule(EruptDemoAutoConfiguration.class);
+    }
+
+    @Override
+    public ModuleInfo info() {
+        return ModuleInfo.builder().name("erupt-Demo").build();
+    }
+
+    @Override
+    public List<MetaMenu> initMenus() {
+        List<MetaMenu> menus = new ArrayList<>();
+        MetaMenu root = MetaMenu.createRootMenu("$Demo", "Demo管理", "fa fa-cogs", 1);
+        menus.add(root);
+        menus.add(MetaMenu.createEruptClassMenu(BlogCategory.class, root, 0));
+        menus.add(MetaMenu.createEruptClassMenu(BlogTag.class, root, 10));
+        menus.add(MetaMenu.createEruptClassMenu(FriendlyLink.class, root, 20));
+        menus.add(MetaMenu.createEruptClassMenu(PicturesCategory.class, root, 30));
+        menus.add(MetaMenu.createEruptClassMenu(Pictures.class, root, 40));
+        menus.add(MetaMenu.createEruptClassMenu(Blog.class, root, 50));
+        return menus;
+    }
+
+}
